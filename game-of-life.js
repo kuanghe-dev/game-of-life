ecc0022bb940b4be1807971c978c8470c4e8a628
Kuang He <kuang.he@gmail.com>
2022-01-10 20:24:47 -0600

    Improve how we initialize various patterns

--- game-of-life.js
+++ game-of-life.js
@@ -234,74 +234,106 @@ function makeRandomState() {
       cellAlive[i][j] = (Math.random() < 0.5);
 }
 
+function addObjectsInCells(objects) {
+  for (let obj of objects)
+    for (let [r, c] of obj.pos)
+      cellAlive[r + obj.row][c + obj.col] = true;
+}
+
 function makeSpaceships(row0=2, col0=2) {
-  let glider = [[0, 1], [1, 2], [2, 0], [2, 1], [2, 2]];
-  let lightWeightSpaceship = [[0, 0], [0, 3], [1, 4], [2, 0], [2, 4], [3, 1],
-                              [3, 2], [3, 3], [3, 4]];
-  let middleWeightSpaceship = [[0, 2], [1, 0], [1, 4], [2, 5], [3, 0], [3, 5],
-                               [4, 1], [4, 2], [4, 3], [4, 4], [4, 5]];
-  let heavyWeightSpaceship = [[0, 2], [0, 3], [1, 0], [1, 5], [2, 6], [3, 0],
-                              [3, 6], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6]];
+  const glider = {
+    row: 2,
+    col: 2,
+    pos: [[0, 1], [1, 2], [2, 0], [2, 1], [2, 2]],
+  };
+  const lightWeightSpaceship = {
+    row: 14,
+    col: 2,
+    pos: [[0, 0], [0, 3], [1, 4], [2, 0], [2, 4], [3, 1], [3, 2], [3, 3], [3, 4]],
+  };
+  const middleWeightSpaceship = {
+    row: 26,
+    col: 2,
+    pos: [[0, 2], [1, 0], [1, 4], [2, 5], [3, 0], [3, 5], [4, 1], [4, 2],
+          [4, 3], [4, 4], [4, 5]],
+  }
+  const heavyWeightSpaceship = {
+    row: 38,
+    col: 2,
+    pos: [[0, 2], [0, 3], [1, 0], [1, 5], [2, 6], [3, 0], [3, 6], [4, 1],
+          [4, 2], [4, 3], [4, 4], [4, 5], [4, 6]],
+  };
  
-  let objects = [
+  addObjectsInCells([
     lightWeightSpaceship,
     middleWeightSpaceship,
     heavyWeightSpaceship,
     glider,
-  ];
-
-  for (let obj of objects) {
-    for (let [row, col] of obj)
-      cellAlive[row + row0][col + col0] = true;
-    row0 += 12;
-  }
+  ]);
 }
 
 function makeStillLifes() {
-  let block = [[0, 0], [0, 1], [1, 0], [1, 1]];
-  let beehive = [[0, 1], [0, 2], [1, 0], [1, 3], [2, 1], [2, 2]];
-  let loaf = [[0, 1], [0, 2], [1, 0], [1, 3], [2, 1], [2, 3], [3, 2]];
-  let boat = [[0, 0], [0, 1], [1, 0], [1, 2], [2, 1]];
-  let tub = [[0, 1], [1, 0], [1, 2], [2, 1]];
-
-  let objects = [block, beehive, loaf, boat, tub];
-  let row0 = 10;
-  let col0 = 28;
-  let startPos = [[row0, col0], [row0, col0+8], [row0, col0+17], [row0, col0+26], [row0, col0+35]];
-
-  let idx = 0;
-  for (let obj of objects) {
-    for (let [row, col] of obj)
-      cellAlive[row + startPos[idx][0]][col + startPos[idx][1]] = true;
-    idx += 1
+  const block = {
+    row: 10,
+    col: 28,
+    pos: [[0, 0], [0, 1], [1, 0], [1, 1]],
+  };
+  const beehive = {
+    row: 10,
+    col: 36,
+    pos: [[0, 1], [0, 2], [1, 0], [1, 3], [2, 1], [2, 2]],
+  };
+  const loaf = {
+    row: 10,
+    col: 45,
+    pos: [[0, 1], [0, 2], [1, 0], [1, 3], [2, 1], [2, 3], [3, 2]],
   }
+  const boat = {
+    row: 10,
+    col: 54,
+    pos: [[0, 0], [0, 1], [1, 0], [1, 2], [2, 1]],
+  };
+  const tub = {
+    row: 10,
+    col: 63,
+    pos: [[0, 1], [1, 0], [1, 2], [2, 1]],
+  };
+
+  addObjectsInCells([block, beehive, loaf, boat, tub]);
 }
 
 function makeOscillators() {
-  let blinker = [[0, 0], [0, 1], [0, 2]];
-  let toad = [[0, 1], [0, 2], [0, 3], [1, 0], [1, 1], [1, 2]];
-  let beacon = [[0, 0], [0, 1], [1, 0], [2, 3], [3, 2], [3, 3]];
-  let pulsar = [[0, 2], [0, 3], [0, 4], [0, 8], [0, 9], [0, 10],
-                [2, 0], [2, 5], [2, 7], [2, 12],
-                [3, 0], [3, 5], [3, 7], [3, 12],
-                [4, 0], [4, 5], [4, 7], [4, 12],
-                [5, 2], [5, 3], [5, 4], [5, 8], [5, 9], [5, 10],
-                [7, 2], [7, 3], [7, 4], [7, 8], [7, 9], [7, 10],
-                [8, 0], [8, 5], [8, 7], [8, 12],
-                [9, 0], [9, 5], [9, 7], [9, 12],
-                [10, 0], [10, 5], [10, 7], [10, 12],
-                [12, 2], [12, 3], [12, 4], [12, 8], [12, 9], [12, 10]];
-  let Pentadecathlon = [[0, 2], [1, 1], [1, 2], [1, 3], [2, 0], [2, 1], [2, 2],
-                        [2, 3], [2, 4], [9, 0], [9, 1], [9, 2], [9, 3], [9, 4],
-                        [10, 1], [10, 2], [10, 3], [11, 2]];
+  const blinker = {
+    row: 11,
+    col: 15,
+    pos: [[0, 0], [0, 1], [0, 2]],
+  };
+  const toad = {
+    row: 23,
+    col: 15,
+    pos: [[0, 1], [0, 2], [0, 3], [1, 0], [1, 1], [1, 2]],
+  };
+  const beacon = {
+    row: 35,
+    col: 15,
+    pos: [[0, 0], [0, 1], [1, 0], [2, 3], [3, 2], [3, 3]],
+  };
+  const pulsar = {
+    row: 24,
+    col: 34,
+    pos: [[0, 2], [0, 3], [0, 4], [0, 8], [0, 9], [0, 10], [2, 0], [2, 5],
+          [2, 7], [2, 12], [3, 0], [3, 5], [3, 7], [3, 12], [4, 0], [4, 5],
+          [4, 7], [4, 12], [5, 2], [5, 3], [5, 4], [5, 8], [5, 9], [5, 10],
+          [7, 2], [7, 3], [7, 4], [7, 8], [7, 9], [7, 10], [8, 0], [8, 5],
+          [8, 7], [8, 12], [9, 0], [9, 5], [9, 7], [9, 12], [10, 0], [10, 5],
+          [10, 7], [10, 12], [12, 2], [12, 3], [12, 4], [12, 8], [12, 9], [12, 10]],
+  };
+  const Pentadecathlon = {
+    row: 24,
+    col: 61,
+    pos: [[0, 2], [1, 1], [1, 2], [1, 3], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4],
+          [9, 0], [9, 1], [9, 2], [9, 3], [9, 4], [10, 1], [10, 2], [10, 3], [11, 2]],
+  };
 
-  let objects = [blinker, toad, beacon, pulsar, Pentadecathlon];
-  let startPos = [[11, 15], [23, 15], [35, 15], [24, 34], [24, 61]];
-
-  let idx = 0;
-  for (let obj of objects) {
-    for (let [row, col] of obj)
-      cellAlive[row + startPos[idx][0]][col + startPos[idx][1]] = true;
-    idx += 1
-  }
+  addObjectsInCells([blinker, toad, beacon, pulsar, Pentadecathlon]);
 }
